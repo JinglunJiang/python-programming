42534345e5ad0ee66eba533f1931cb19580ecaca
Jinglun (Noella) Jiang <jinglunjiang@gmail.com>
2023-11-12 23:00:21 +0100

    Delete homework-6-JinglunJiang-main directory

--- homework-6-JinglunJiang-main/problem2/problem2.py
+++
@@ -1,9 +0,0 @@
-
-
-def main():
-    # this is the main function, which is the entry point for the program
-    # you should write your code in methods and/or functions and have the
-    # main function call those functions
-
-if __name__ == "__main__":
-    main()
